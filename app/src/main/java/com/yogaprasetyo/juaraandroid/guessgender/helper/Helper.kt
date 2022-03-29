b80d3041e54817d3698b38691aecb1915d0f0b91
Yoga Prasetyo <jobsforyoga@gmail.com>
2022-03-29 11:11:16 +0700

    Add seconds on date format pattern

--- app/src/main/java/com/yogaprasetyo/juaraandroid/guessgender/helper/Helper.kt
+++ app/src/main/java/com/yogaprasetyo/juaraandroid/guessgender/helper/Helper.kt
@@ -11,7 +11,7 @@ class Helper {
          * 2022, 1 Jan 01:20 a/p.m
          * */
         fun getCurrentDate(): String {
-            val pattern = "yyyy, d MMM hh:mm a"
+            val pattern = "yyyy, d MMM hh:mm:ss a"
             val dateFormat = SimpleDateFormat(pattern, Locale.getDefault())
             return dateFormat.format(Date())
         }
