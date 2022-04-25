c723860acbb1d8316b00cdb20d85bec3ffe48959
Yoga Prasetyo <jobsforyoga@gmail.com>
2022-04-25 10:19:14 +0700

    Add lint and modified documentation

--- app/src/main/java/com/yogaprasetyo/juaraandroid/guessgender/SplashScreenActivity.kt
+++ app/src/main/java/com/yogaprasetyo/juaraandroid/guessgender/SplashScreenActivity.kt
@@ -1,5 +1,6 @@
 package com.yogaprasetyo.juaraandroid.guessgender
 
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
@@ -8,8 +9,9 @@ import com.yogaprasetyo.juaraandroid.guessgender.ui.MainActivity
 
 /**
  * Handle splash screen for first open app
- * this splash screen work on API 12
+ * this splash screen work on Android 12
  * */
+@SuppressLint("CustomSplashScreen")
 class SplashScreenActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         startActivity(Intent(this, MainActivity::class.java))
